8a17bf84ec1975ce96552f92dec96c8770a2ec58
Ankur chauhan <ankurc2001@gmail.com>
2020-05-04 14:09:36 +0530

    port okay

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express()
 const server=http.createServer(app)
 const io = socketio(server)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 const publicDirPath= path.join(__dirname,'../public')
 
 let count =0
